758507740f39c8be20c94dd0540d3e6fb6aac058
MrHoran8120 <58581424+MrHoran8120@users.noreply.github.com>
2022-06-25 22:50:02 +1000

    Add project files.

---
+++ DemoLocalStorage/Student.cs
@@ -0,0 +1,46 @@
+﻿using Blazored.LocalStorage;
+
+
+namespace DemoLocalStorage
+{
+    public class Student
+    {
+
+       
+
+
+        public static List<Student> students = new List<Student>();
+
+        public string Name { get; set; }
+        
+        public int Id { get; set; }
+
+        public DateOnly Dob { get;set;}
+
+        public Student(string name, int id, DateOnly dob)
+        {
+            Name = name;
+            Id = id;
+            Dob = dob;
+        }
+
+        public static void printAllStudents()
+        {
+            foreach (var student in students)
+                Console.WriteLine("Student " + student.Name);
+        }
+
+        public static string serializeStudentsList()
+        {
+            string serializedString = System.Text.Json.JsonSerializer.Serialize(Student.students);
+
+            Console.WriteLine("The list is serialized to " + serializedString);
+            return serializedString;
+        }
+        public static  void deserializeStudentListAsync(string mySerializedList)
+        {
+            Student.students.Clear();
+            Student.students = System.Text.Json.JsonSerializer.Deserialize<List<Student>>(mySerializedList);
+        }
+    }
+}
